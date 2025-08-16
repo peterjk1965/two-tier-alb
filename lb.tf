9d820b3c714849ab3d611ad383fffa5eb77561e3
Peter <peter.j.k@hotmail.com>
2025-08-16 15:49:25 -0500

    Update lb.tf

--- lb.tf
+++ lb.tf
@@ -110,7 +110,7 @@ resource "aws_autoscaling_group" "web_asg" {
   }
 }
 
-# Auto Scaling Policy (CPU-based example)
+# Auto Scaling Policy (CPU-based)
 resource "aws_autoscaling_policy" "scale_out" {
   name                   = "scale-out-policy"
   autoscaling_group_name = aws_autoscaling_group.web_asg.name
